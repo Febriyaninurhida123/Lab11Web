ff3bd80ba51e9e59c88895bf7c5a22449b85cbec
Febriyaninurhida123 <sutarmiprihatna@gmail.com>
2023-12-16 13:10:02 +0700

    first commit

---
+++ template/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../template/style.css">
+</head>
+
+<body>
+    <footer>
+        <div>
+            <p>&copy; 2023, Teknik Informatika, <a href="https://wa.me/6285724667129" style="text-decoration: none;">Febriyani Nurhida</a>, Universitas Pelita Bangsa</p>
+        </div>
+    </footer>
+</body>
+
+</html>
